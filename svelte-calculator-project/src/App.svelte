0c331a70813356517947db129092376b8e648644
gjghwjdgh <gjghwjdgh@naver.com>
2021-10-17 17:35:10 +0900

    feat: 계산기 프로젝트 layout 제작

---
+++ svelte-calculator-project/src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import CalculatorButton from "./components/CalculatorButton.svelte";
+</script>
+
+<div class="container">
+  {#each Array(9) as _, i}
+    <CalculatorButton name={i + 1} />
+  {/each}
+  <!-- {#each <CalculatorButton name=1/>} -->
+</div>
+
+<style>
+  .container {
+    display: grid;
+    background-color: #8ca0ff;
+    width: 100%;
+    grid-auto-rows: 50px;
+    grid-template-columns: repeat(3, 1fr);
+    column-gap: 20px;
+  }
+</style>
---
+++ svelte-calculator-project/src/components/CalculatorButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let name;
+</script>
+
+<button>
+  {name}
+</button>
+
+<style>
+  Button {
+    padding: 5pt;
+    background-color: blue;
+    color: aliceblue;
+  }
+</style>
